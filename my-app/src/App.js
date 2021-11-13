3f1b39c519bf7b2876879a490ce5480f81e36047
ignaciodacio <ignaciodacio@gmail.com>
2021-11-13 18:42:20 -0300

    piano working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -46,13 +46,40 @@ navigator.requestMIDIAccess().then(function(webmidi) {
     if (err) console.log(err);
   }
 );
+//JZZ.synth.Tiny.register('Synth');
+var port = navigator.JZZ().openMidiOut().or(function(){alert('Cannot open MIDI port!');});
 
+var midi={65:58,90:59,88:60,68:61,67:62,70:63,86:64,66:65,72:66,78:67,74:68,77:69,75:70,188:71,190:72,186:73,59:73,191:74,222:75};
+document.onkeydown=function(e){
+ var e=window.event || e;
+ var m=midi[e.keyCode];
+ if(m){
+  var k=document.getElementById(m);
+  if(k && k.className!="on"){
+   port.send([0x90,m,0x7f]);
+   k.className="on";
+  }
+  return false;
+ }
+}
+document.onkeyup=function(e){
+ var e=window.event || e;
+ var m=midi[e.keyCode];
+ if(m){
+  var k=document.getElementById(m);
+  if(k && k.className!=""){
+   port.send([0x80,m,0]);
+   k.className="";
+  }
+  return false;
+ }
+}
 
   return (
     <div className="App">
       <header className="App-header">
           <Piano>
-            <span id='58'>A</span><span class='blank'>S</span><span id='61'>D</span><span id='63'>F</span><span class='blank'>G</span><span id='66'>H</span><span id='68'>J</span><span id='70'>K</span><span class='blank'>L</span><span id='73'>:<br/>;</span><span id='75'>"<br/>'</span>
+            <span id='58'>A</span><span className='blank'>S</span><span id='61'>D</span><span id='63'>F</span><span className='blank'>G</span><span id='66'>H</span><span id='68'>J</span><span id='70'>K</span><span className='blank'>L</span><span id='73'>:<br/>;</span><span id='75'>"<br/>'</span>
         </Piano>
         <Piano>
             <span id='59'>Z</span><span id='60'>X</span><span id='62'>C</span><span id='64'>V</span><span id='65'>B</span><span id='67'>N</span><span id='69'>M</span><span id='71'><br/>,</span><span id='72'><br/>.</span><span id='74'>?<br/>/</span>
