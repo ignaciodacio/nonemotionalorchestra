8e21e96d64d347655f7ee17d3f113818084a2ae8
ignaciodacio <ignaciodacio@gmail.com>
2021-11-13 18:11:25 -0300

    was

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import
 
 function App() {
+
+  const Button = styled.a`
+  /* This renders the buttons above... Edit me! */
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 11rem;
+  background: transparent;
+  color: white;
+  border: 2px solid white;
+
+  /* The GitHub button is a primary button
+   * edit this to target it specifically! */
+  ${props => props.primary && css`
+    background: white;
+    color: black;
+  `}
+`
+
   return (
     <div className="App">
       <header className="App-header">
@@ -23,3 +44,5 @@ function App() {
 }
 
 export default App;
+
+
